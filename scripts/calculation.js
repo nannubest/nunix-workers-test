6a3105fc1fe0020808fa93308032d4170f6883a3
Nahir Acosta <nahir.95@gmail.com>
2012-01-13 11:17:35 -0300

    first commit!

---
+++ scripts/calculation.js
@@ -0,0 +1,11 @@
+function pi(digit){
+	var p=parseInt,a=10000,c=p(digit*3.5),b=d=e=g=0,f=[],s=r=""
+	for(;b-c;)f[b++]=p(a/5)
+	for(;d=0,g=c*2;c-=14,s=("0000").concat(p(e)+p(d/a)),r+=s.substr(s.length-4),e=d%a)
+	for(b=c;d+=(f[b]?f[b]:0)*a,f[b]=d%--g,d= p(d/g--),--b;d*=b);
+	return r
+}
+sleep(1*1000); // 1 seg
+postMessage({data: "Procesando", func: 'estado'});
+postMessage({data: pi(10000), func: 'pi'});
+postMessage({data: "¡Listo!", func: 'estado'});
